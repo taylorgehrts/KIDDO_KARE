08cd17b36c6e9e8f2e828adbb63d3b7eaa984cc7
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-08-08 15:58:28 -0700

    re commiting to only include working code

--- models/index.js
+++ models/index.js
@@ -10,14 +10,14 @@ ParentInfo.belongsTo(User, { foreignKey: 'userId' });
 User.hasOne(SitterInfo, { foreignKey: 'userId' });
 SitterInfo.belongsTo(User, { foreignKey: 'userId' });
 
-ParentInfo.hasMany(Job, { foreignKey: 'parentId' });
-Job.belongsTo(ParentInfo, { foreignKey: 'parentId' });
+// ParentInfo.hasMany(Job, { foreignKey: 'parentId' });
+// Job.belongsTo(ParentInfo, { foreignKey: 'parentId' });
 
-Job.hasOne(SitterInfo, { foreignKey: 'workerId' });
-SitterInfo.belongsTo(Job, { foreignKey: 'workerId' });
+// Job.hasOne(SitterInfo, { foreignKey: 'workerId' });
+// SitterInfo.belongsTo(Job, { foreignKey: 'workerId' });
 
-ParentInfo.hasMany(ChildInfo, { foreignKey: 'parentId' });
-ChildInfo.belongsTo(ParentInfo, { foreignKey: 'parentId' });
+// ParentInfo.hasMany(ChildInfo, { foreignKey: 'parentId' });
+// ChildInfo.belongsTo(ParentInfo, { foreignKey: 'parentId' });
 
 module.exports = {
     User, ParentInfo, SitterInfo, ChildInfo,
