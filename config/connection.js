909358414f80dbeb3ddd2737416d11128d30610a
taylorgehrts <taylorgehrts@gmail.com>
2023-08-07 20:30:06 -0700

    initial commit, created basic file structure, added a connection file with commented out connection for mac, .env file schema file

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    //file path for taylor to run on mac
+    // dialectOptions: {
+    //   socketPath: '/tmp/mysql.sock'
+    // }
+  }
+);
+
+module.exports = sequelize;
