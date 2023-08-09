0c4a8ed539c0e5bd9caecd37df1a1bee594a222a
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-08-09 15:17:58 -0700

    /login annd /logout routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const { createUser } = require('../../models/lib/create');
+const User = require('../../models/User');
 
-// POST a user
+// Create a user and log them in
 router.post('/', async (req, res) => {
     const data = req.body;
     const userData = {
@@ -38,10 +39,46 @@ router.post('/', async (req, res) => {
             req.session.userId = result.user.id;
             req.session.loggedIn = true;
 
-            res.status(200).json({ message: 'POST /api/user successful! Logged in!', data: result});
+            res.status(200).json({ message: 'POST /api/users successful! Logged in!', data: result});
         });
     } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const name = req.body.userName;
+    const email = req.body.email;
+    const clearTextPassword = req.body.password;
+    let user;
+
+    if (name) {
+        user = await User.findOne({ where: { userName: name } });
+    } else if (email) {
+        user = await User.findOne({ where: { email: email } });
+    }
+
+    if (!user) {
+        return res.status(400).json({ message: "Incorrect username or password" });
+    }
+
+    if (user.comparePasswordHash(clearTextPassword)) {
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.loggedIn = true;
+
+            res.status(200).json({ message: 'Logged in!' });
+        });
+    } else {
+        return res.status(400).json({ message: "Incorrect password" });
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => res.status(204).json({ message: "Logged out!" }));
+    } else {
+        res.status(404).end();
     }
 });
 
