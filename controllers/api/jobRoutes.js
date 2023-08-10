49501007d028012d7ae78894f043e50954a2c8bf
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-08-10 15:19:50 -0700

    GET /sitters route

--- controllers/api/jobRoutes.js
+++ controllers/api/jobRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('sequelize');
-const { Op } = require('sequelize');
+const Op = sequelize.Op;
 
 const { Job, ParentInfo, User } = require('../../models');
 
---
+++ controllers/api/sitterRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+
+const { SitterInfo, User } = require('../../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op
+
+router.get('/', async (req, res) => {
+    const filters = req.query;
+    let options = { where: {}, include: User};
+
+    if (filters.yearsExperience) {
+        options.where.yearsExperience = { [Op.gte]: filters.yearsExperience };
+    }
+
+    if (filters.qualifications) {
+        options.where[Op.or] = [];
+        filters.qualifications.forEach(qualification => {
+            options.where[Op.or].push({ qualifications: sequelize.where(sequelize.fn('LOWER', sequelize.col('qualifications')), 'LIKE', '%' + qualification + '%')  })
+        });
+    }
+
+    try {
+        const sitters = await SitterInfo.findAll(options)
+        const result = sitters.map(sitter => sitter.toJSON());
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+    res.status(200).json(result);
+});
+
+module.exports = router;
