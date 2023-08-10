604d8766f409a2a8b675542d6ec933fe767370ae
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-08-10 14:18:55 -0700

    GET /parent/:id route

--- controllers/page/parentRoutes.js
+++ controllers/page/parentRoutes.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
 
+const { User, ParentInfo } = require('../../models');
+const { getChildren } = require('../../models/lib/get');
 
+router.get('/:id', async (req, res) => {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId);
+    const parent = await user.getParentInfo();
+    const jobs = parent.getJobs();
+    const children = await getChildren(userId);
+
+    res.render('parent', { user, parent, jobs, children });
+});
 
 module.exports = router;
