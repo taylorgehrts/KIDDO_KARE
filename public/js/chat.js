fd23cc3cf3c536bce85d6a90a1b0af72e80e9bd4
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-08-14 16:27:07 -0700

    Client-side JS for chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,9 +3,24 @@ var socket = io();
 const chatFormSubmitHandler = event => {
     event.preventDefault();
 
-    const message = document.getElementById('chat-box').value;
+    const messageBox = document.getElementById('chat-box');
+    const message = messageBox.value;
     const chatWindow = document.getElementById('chat-window');
 
+    // Emit socket.io event
+    if (message) {
+        socket.emit('message', message);
+        messageBox.value = '';
+    }
+
+    // Recieve chat messages
+    socket.on('message', msg => {
+        var msgDiv = document.createElement('div');
+        msgDiv.textContent = msg;
+        chatWindow.append(msgDiv);
+
+        msgDiv.scrollTo({ top: msgDiv.scrollHeight });
+    });
 };
 
 document.getElementById('chat-form').addEventListener('submit', chatFormSubmitHandler);
