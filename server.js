932b33160ab16f44e5a887eead1b8cc2b55ce4eb
efogerson1 <efogerson2@gmail.com>
2023-08-14 15:18:11 -0700

    Add new or existing users, changes made to login.js, server.js

--- server.js
+++ server.js
@@ -26,6 +26,16 @@ const sess = {
 app.use(session(sess));
 // Sync sequelize models to the database, then turn on the server 
 //TURN OFF force true for deployment
+// EF - route for saving sessions, successful login
+app.post('/api/users/save-session', (req, res) => {
+  if (req.session.userId && req.session.loggedIn) {
+      req.session.save(() => {
+          res.status(200).json({ message: 'Session saved' });
+      });
+  } else {
+      res.status(400).json({ message: 'Session not saved' });
+  }
+});
 sequelize.sync({ force: true }).then(() => {
   // The `force: false` option ensures that existing data in the tables won't be dropped.
   // Set `force: true` to drop existing data and re-create the tables.
